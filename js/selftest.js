182406be598119e669bb30903644b6a72cfc2ee4
Joakim Ohlrogge <joakim.ohlrogge@agical.com>
2013-10-30 00:07:59 +0100

    possible to run simple cases

--- js/selftest.js
+++ js/selftest.js
@@ -16,20 +16,77 @@ var failingSpec =  function(){
     return deferred.promise;
 }
 
-Q.when(tattler.run(
-    [passingSpec,
-     failingSpec],
-    function(result){
+
+function testRun(name, specs, expectedProgress, expectedResults) {
+    return function() {
+        var progressLog = [];
+        var result = Q.when(tattler.run(specs)).
+            progress(function(progress){
+                progressLog = progressLog.concat(progress);
+            }).
+            then(function(theResult){
+                assert.deepEqual(progressLog, expectedProgress);
+                assert.deepEqual(theResult, expectedResults);
+            });
+        result.name = name;
         return result;
     }
-)).
-progress(function(progress){console.log("progress",progress);}).
-done(function(theResult){
-    assert.equal(theResult[0].passed, true);
-    assert.equal(theResult[0].name, 'simple test');
-    assert.equal(theResult[0].result, 'result');
+}
 
-    assert.equal(theResult[1].passed, false);
-    assert.equal(theResult[1].name, 'failing test');
-    assert.equal(theResult[1].result, 'error');
-});
+
+Q.when(tattler.run([
+    testRun("one passing",
+            passingSpec,
+            [tattler.progress.started('simple test'),
+             tattler.progress.success('simple test')],
+            [{passed: true,
+              name: 'simple test',
+              result: 'result'}]),
+    testRun("one failing",
+            failingSpec,
+            [tattler.progress.started('failing test'),
+             tattler.progress.failure('failing test')],
+            [{passed: false,
+              name: 'failing test',
+              result: 'error'}]),
+    testRun("one passing, one failing",
+            [passingSpec,
+             failingSpec],
+            [tattler.progress.started('simple test'),
+             tattler.progress.started('failing test'),
+             tattler.progress.success('simple test'),
+             tattler.progress.failure('failing test')],
+            [{passed: true,
+              name: 'simple test',
+              result: 'result'},
+             {passed: false,
+              name: 'failing test',
+              result: 'error'}]
+           )])).
+    progress(function(progress){
+        if (progress.status !== 'started') {
+            process.stdout.write(progress.status === 'success'? '.':'F');
+        }
+    }).done(function(result){
+        console.log();
+        var summary = result.reduce(function(previous, current) {
+            var sum = previous;
+            if (current.passed) {
+                sum.passed += 1;
+            }
+            else {
+                sum.failed += 0;
+            }
+            return sum;
+        }, {passed:0, failed:0});
+        result.filter(
+            function(elem){
+                return !elem.passed;
+            }).
+            forEach(function(elem) {
+                console.log("failed: ", elem.name);
+                console.log(elem.result);
+            });
+        console.log();
+        console.log(summary);
+    });
--- js/tattler.js
+++ js/tattler.js
@@ -1,18 +1,33 @@
+function status(name, status) {
+    return {name:name, status:status};
+};
+
+var progress = {
+    started: function(name){return status(name, 'started');},
+    success: function(name){return status(name, 'success');},
+    failure: function(name){return status(name, 'failure');}
+};
+
 var tattler = function(Q) {
-
-    var run = function(jobs, result){ 
-
+    var run = function(jobs){ 
         var runJobs = function(deferred) {
+            if(!jobs.map) {
+                jobs = [jobs];
+            }
             var results = jobs.map(
                 function(job){
                     var result = job();
-                    deferred.notify({name:result.name, status:'started'});
+                    deferred.notify(progress.started(result.name));
                     return Q.when(result).
-                        then(function(res){return {name:result.name, passed:true, result:res}},
-                             function(res){return {name:result.name, passed:false, result:res}}).
-                        fin(function(res){
-                            deferred.notify({name:result.name, status:'finished'});
-                            return res;});
+                        then(
+                            function(res){
+                                deferred.notify(progress.success(result.name));
+                                return {name:result.name, passed:true, result:res};
+                            },
+                             function(res){
+                                 deferred.notify(progress.failure(result.name));
+                                 return {name:result.name, passed:false, result:res}
+                             })
                 });
             Q.all(results).then(function(res){
                 return deferred.resolve(res);
@@ -33,3 +48,4 @@ var tattler = function(Q) {
 
 var res = tattler(require('q'));
 exports.run = res.run;
+exports.progress = progress;
