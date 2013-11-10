8994f5adb89be2d70ae85c103bd1e868839a9cf1
Joakim Ohlrogge <joakim.ohlrogge@agical.com>
2013-11-10 22:53:10 +0100

    assert works in browser

--- js/selftest.js
+++ js/selftest.js
@@ -98,10 +98,30 @@
         )
     }
     else {
+        function deepEqual(expected, actual, path){
+            if (typeof expected !== typeof actual) {
+                throw new Error(expected + " and " + actual +" don't have the same type");
+            }
+            if (typeof expected === 'object') {
+                var key;
+                for (key in expected) {
+                    deepEqual(expected[key], actual[key], (path || "") +"."+key);
+                }
+            }
+            else if (expected !== actual) {
+                function AssertionError() {
+                 return {name:'AssertionError',
+                           path: path,
+                           expected:expected,
+                           actual: actual}};
+                throw new AssertionError();
+            }
+        }
+        
         specs(
             Q,
             tattler(Q),
-            undefined
+            {deepEqual:deepEqual}
         )
     }
 })();
