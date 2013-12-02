140df2064c5445e408ef3b56fd40718fbda6fb87
Joakim Ohlrogge <joakim.ohlrogge@agical.com>
2013-12-02 22:43:59 +0100

    tested passing dependencies

--- js/selftest.js
+++ js/selftest.js
@@ -22,7 +22,17 @@
             }
 
             var dependsOnFailingSpec = tattler.task("depends on failing", [failingSpec], function(fromDependent){
-                Q.resolve("will never happen");
+                return Q.resolve("will never happen");
+            });
+
+            var dependsOnPassingSpec = tattler.task("depends on passing", [passingSpec], function(fromDependent){
+                return Q.resolve("From dependent: " + fromDependent);
+            });
+
+            var dependsOnManyPassingSpecs = tattler.task("depends on two passing", [passingSpec, passingSpecWithAName], function(fromDependent, other){
+                console.log("Passed???", fromDependent, other);
+
+                return Q.resolve("From dependent: " + fromDependent+" and "+other);
             });
 
             function testRun(name, specs, expectedResults) {
@@ -91,19 +101,51 @@
                              }
                             }
                            ),
-                    testRun("run dependent",
-                           [dependsOnFailingSpec],
-                           { 
-                               'failing test':{
-                                   passed: false,
-                                   name: 'failing test',
-                                   result: 'error'
-                               },
-                               'depends on failing': {
-                                   passed:'skipped',
-                                   name:'depends on failing'
-                               }
-                           })
+                    testRun("run failing dependent",
+                            [dependsOnFailingSpec],
+                            { 
+                                'failing test':{
+                                    passed: false,
+                                    name: 'failing test',
+                                    result: 'error'
+                                },
+                                'depends on failing': {
+                                    passed:'skipped',
+                                    name:'depends on failing'
+                                }
+                            }),
+                    testRun("run passing dependent",
+                            [dependsOnPassingSpec],
+                            {
+                                'simple test':{
+                                    passed: true,
+                                    name: 'simple test',
+                                    result: 'result'
+                                },
+                                'depends on passing': {
+                                    passed:true,
+                                    name:'depends on passing',
+                                    result: 'From dependent: result'
+                                }
+                            }),
+                    testRun("run passing depends on many",
+                            [dependsOnManyPassingSpecs],
+                            {
+                                'simple test':{
+                                    name: 'simple test',
+                                    passed: true,
+                                    result: 'result'
+                                },
+                                'passingSpecWithAName':{
+                                    name: 'passingSpecWithAName',
+                                    passed: true,
+                                    result: 'a result'},
+                                'depends on two passing': {
+                                    name:'depends on two passing',
+                                    passed:true,
+                                    result: 'From dependent: result and a result'
+                                }
+                            })
                 ]),
                 
                 function(eventuallyAcc, eventuallyResult){
@@ -144,7 +186,7 @@
     else {
         function deepEqual(expected, actual, path){
             if (typeof expected !== typeof actual) {
-                throw new Error(expected + " and " + actual +" don't have the same type");
+                throw new Error(expected + " and " + actual + " in " +path+" don't have the same type");
             }
             if (typeof expected === 'object') {
                 var key;
